0526d1e3dd8434138084f5c98a382ea056f14526
risanjayy <risanjayy@gmail.com>
2025-02-17 13:51:34 +0700

    add-experiences

---
+++ src/components/Experience.jsx
@@ -0,0 +1,39 @@
+import { EXPERIENCES } from "../constants";
+
+function Experience() {
+  return (
+    <div className="border-b border-neutral-900 pb-4">
+      <h2 className="my-20 text-center text-4xl">Experiences</h2>
+      <div>
+        {EXPERIENCES.map((experience, index) => (
+          <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+            {/* Kiri */}
+            <div className="w-full lg:w-1/4">
+              <p className="mb-2 text-sm text-neutral-400">{experience.year}</p>
+            </div>
+            {/* Kanan */}
+            <div className="w-full max-x-xl lg:w-3/4">
+              <h6 className="mb-2 font-semibold">
+                {experience.role} -{" "}
+                <span className="text-sm text-purple-100">
+                  {experience.company}
+                </span>
+              </h6>
+              <p className="mb-4 text-neutral-400">{experience.description}</p>
+              {experience.technologies.map((tech, index) => (
+                <span
+                  key={index}
+                  className="mr-2 mt-4 bg-neutral-900 px-2 py-1 rounded font-medium text-sm text-purple-800"
+                >
+                  {tech}
+                </span>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Experience;
